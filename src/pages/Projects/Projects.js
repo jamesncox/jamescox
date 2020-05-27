694d3c26c5b267af0f4904e70d100b9cf3ff71fd
jamesncox <jamesncox@yahoo.com>
2020-05-27 15:18:14 -0500

    Add icons to project page, delete unused files

--- src/pages/Projects/Projects.js
+++ src/pages/Projects/Projects.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Layout from '../../components/Layout';
+import { Code20, Wikis20 } from '@carbon/icons-react'
 import { SectionTitle, Pill } from '../../styles';
 import { ProjectItem, ProjectTitle, SkillContainer } from './styles';
 
@@ -11,15 +12,21 @@ const Projects = ({ user }) => {
                 <ul>
                     {user.projects.map((project, i) => (
                         <ProjectItem key={i}>
-                            <ProjectTitle>{project.name}</ProjectTitle>
-                            <a href={project.website}>{project.website}</a><br></br>
-                            <a href={project.githubUrl}>{project.githubUrl}</a>
+                            <ProjectTitle>{project.name}
+                                <a href={project.website} style={{ marginLeft: "10px" }}><Wikis20 /></a>
+                                <a href={project.githubUrl} style={{ marginLeft: "10px" }}><Code20 /></a>
+                            </ProjectTitle>
                             <p>{project.summary}</p>
                             <SkillContainer>
                                 {[...project.languages, ...project.libraries].map((item, j) => (
                                     <Pill key={j}>{item}</Pill>
                                 ))}
                             </SkillContainer>
+                            {/* {[...project.images].map(image => (
+                                image.resolutions.map((resolution, k) => (
+                                    <img key={k} src={resolution.thumbnail.url} alt={resolution.thumbnail.url} />
+                                ))
+                            ))} */}
                         </ProjectItem>
                     ))}
                 </ul>
